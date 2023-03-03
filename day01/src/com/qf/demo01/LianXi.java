6f245d78d4b636b161e34d7941224546784d15a7
copycool <gtbetter666@163.com>
2023-03-03 15:28:09 +0800

    sdad

--- day01/src/com/qf/demo01/LianXi.java
+++ day01/src/com/qf/demo01/LianXi.java
@@ -8,17 +8,13 @@ public class LianXi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 9;
-//		double y = Math.sqrt(x);
-//		System.out.println(y);
-		Scanner sca = new Scanner(System.in);
-		System.out.println("请输入i:");
-		int i = sca.nextInt();
-		System.out.println("请输入f:");
-		float f = sca.nextFloat();
-		int y = Math.round(f);// round 返回类型是long需要强制类型转换 还可以直接返回int
-		System.out.println(f + "f+i");
-		System.out.println(y);
+		int j = 1;
+		int i;
+		j = j++;
+		i = j++;
+		int result;
+		result = j + ++j * i++;
+		System.out.println(result);
 	}
 
 }
