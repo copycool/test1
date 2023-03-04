b720f16717a42421b67fb17c701d229306222174
copycool <gtbetter666@163.com>
2023-03-04 11:21:52 +0800

    更改

--- day01/src/com/qf/zuoye/SiKao02.java
+++ day01/src/com/qf/zuoye/SiKao02.java
@@ -3,11 +3,11 @@ package com.qf.zuoye;
 import java.util.Scanner;
 import com.qf.demo01.MathQuYu;
 import com.qf.zuoye.SiKao01;
+
 /**
- * 某个公司采⽤公⽤电话传递数据，数据是四位的整数，在传递过程中是加密的， 
-加密规则如下：每位数字都加上3然后除以10的余数代替该数字， 
-再将第⼀位和第四位交换，第⼆位和第三位交换。 
-输⼊四位号码，求加密后的号码为多少？ 
+ * 某个公司采⽤公⽤电话传递数据，数据是四位的整数，在传递过程中是加密的， 加密规则如下：每位数字都加上3然后除以10的余数代替该数字，
+ * 再将第⼀位和第四位交换，第⼆位和第三位交换。 输⼊四位号码，求加密后的号码为多少？
+ * 
  * @author XC
  *
  */
@@ -16,23 +16,26 @@ public class SiKao02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MathQuYu quYu = new MathQuYu();
-		
-		int n = 1234; 
+
+		int n = 1234;
 		int[] a = quYu.quyu(n);
+		for (int i = a.length-1; i >= 0; i--) {
+			a[i] = (a[i] + 3 )%10;
+		}
 		swap(a, 0, 2);
 		swap(a, 1, 3);
 		System.out.println("加密后电话是：");
-		for (int i = a.length-1; i >= 0 ; i--) {
-			System.out.printf(a[i]+"");
+		for (int i = a.length - 1; i >= 0; i--) {
+			System.out.printf(a[i] + "");
 		}
-		
-		
+
 	}
-	public static void swap(int[] a,int i,int j) {
-		int t= a[i];
+
+	public static void swap(int[] a, int i, int j) {
+		int t = a[i];
 		a[i] = a[j];
-		a[j]= t;
-		
+		a[j] = t;
+
 	}
 
 }
